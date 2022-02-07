d1cd69b7c6af826783826e97a03ca0a23767bf9d
eyang <hs.eric.yang@gmail.com>
2022-02-06 23:15:03 -0800

    added projectList

---
+++ src/projectList.js
@@ -0,0 +1,9 @@
+const projectList = [
+    {
+        name: "Aritzia Mock-Up",
+        repoURL: "https://github.com/oPhreak/atzAssessJrFED",
+        description: "A case assessment to convert a mockup into a responsive web page. Built with React."
+    }
+];
+
+export default projectList;
